9d93a62ce4ca261b4c82cdcb4c802a8c122aa123
Carlos12001 <carlos.andres12001@gmail.com>
2020-11-09 21:35:44 -0600

    LastChange

--- src/main/java/demo/starwars/Main.java
+++ src/main/java/demo/starwars/Main.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class Main {
     private static final ObjectMapper parser = new ObjectMapper();
     public static void main(String[] args) throws IOException {
-        Content response = Request.Get("https://swapi.dev/api/people/").execute().returnContent();
+        Content response = Request.get("https://swapi.dev/api/people/").execute().returnContent();
 //        ("https://swapi.dev/api/people/").execute().returnContent();
         ApiResponse<People> parsedResponse = parser.readValue(response.asString(), new TypeReference<ApiResponse<People>>() {});
         for (People person : parsedResponse.getResults()) {
